705d953dec904030264908216a43778dc47d5d07
Girish Mahabir <girish@oceandba.com>
2023-06-05 10:03:13 +0400

    Optimized sed for ssh port.

--- DebianServer_10/jshilder-CIS.sh
+++ DebianServer_10/jshilder-CIS.sh
@@ -521,9 +521,7 @@ echo -n " Securing SSH..."
 sed s/USERNAME/$username/g templates/sshd/sshd_config-CIS > /etc/ssh/sshd_config; echo "OK"
 # get ssh port from user
 echo -n " Type the SSH Port: "; read sshport
-sed -i s/PORT/$sshport/g /etc/ssh/sshd_config; 
-sed -i s/PORT/$sshport/g templates/iptables/iptables-CIS.sh;
-echo "OK"
+sed -i s/PORT/$sshport/g /etc/ssh/sshd_config templates/iptables/iptables-CIS.sh; echo "OK"
 service ssh restart
 
 chown root:root /etc/ssh/sshd_config
